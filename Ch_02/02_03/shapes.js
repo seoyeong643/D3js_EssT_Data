7723e50b68a558392ee9fb9485e1bba1e888afe7
Maia Oh <seoyeong.oh364@gmail.com>
2024-10-07 23:14:18 -0700

    ch2

--- Ch_02/02_03/shapes.js
+++ Ch_02/02_03/shapes.js
@@ -1,5 +1,7 @@
 var dataArray = [5,11,18];
 
-d3.select("body").append("svg")
-    .attr("height","100%")
-    .attr("width","100%");
+var svg = d3.select("body").append("svg").attr("height","100%").attr("width","100%");
+
+svg.selectAll("rect")
+    .data(dataArray)
+    .enter().append("rect")
--- Ch_02/02_04/shapes.js
+++ Ch_02/02_04/shapes.js
@@ -4,4 +4,9 @@ var svg = d3.select("body").append("svg").attr("height","100%").attr("width","10
 
 svg.selectAll("rect")
       .data(dataArray)
-      .enter().append("rect");
+      .enter().append("rect")
+            .attr("height",function(d){ return d*15; })
+            .attr("width","50")
+            .attr("fill","pink")
+            .attr("x",function(d,i){ return 60*i; })
+            .attr("y",function(d,i){ return 300-(d*15); });
